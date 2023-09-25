15aac30b1e7a331db38110cb85031b4d27745213
Lidia Velicia <lvelirui@myuax.com>
2023-09-25 13:40:02 +0200

    añadir funcion objetivo

--- prob_asign_tareas.py
+++ prob_asign_tareas.py
@@ -4,9 +4,16 @@ def assign_tasks():
     # Crear el modelo
     model = cp_model.CpModel()
 
+    # VARIABLES
     # Número de trabajadores y tareas
-    num_workers = 3
-    num_tasks = 3
+    num_workers = 4
+    num_tasks = 4
+
+    # Matriz de tiempo que tarda cada trabajador en realizar cada tarea
+    task_times = [  [3, 5, 2, 7],
+                    [4, 6, 3, 8],
+                    [2, 4, 1, 6],
+                    [5, 7, 2, 9]    ]
 
     # Matriz de asignación trabajador-tarea
     assignment = {}
@@ -14,38 +21,42 @@ def assign_tasks():
         for task in range(num_tasks):
             assignment[(worker, task)] = model.NewBoolVar(f'worker{worker}_task{task}')
 
-    # Cada trabajador realiza una tarea
+
+    # RESTRICCIONES
+    # 1) Cada trabajador realiza una tarea
     for worker in range(num_workers):
         model.Add(sum(assignment[(worker, task)] for task in range(num_tasks)) == 1)
 
-    # Cada tarea es realizada por un trabajador
+    # 2) Cada tarea es realizada por un trabajador
     for task in range(num_tasks):
         model.Add(sum(assignment[(worker, task)] for worker in range(num_workers)) == 1)
 
-    # Restricciones para asignación específica
-    model.Add(assignment[(0, 0)] == 1)  # Trabajador 1 realiza tarea 1
-    model.Add(assignment[(1, 1)] == 1)  # Trabajador 2 realiza tarea 2
-    model.Add(assignment[(2, 2)] == 1)  # Trabajador 3 realiza tarea 3
 
-    # Crear el solucionador
+    # FUNCIÓN OBJETIVO: Minimizar el tiempo total de trabajo
+    total_time = model.NewIntVar(0, sum(max(row) for row in task_times), 'total_time')
+    model.Add(total_time == sum(
+        task_times[worker][task] * assignment[(worker, task)]
+        for worker in range(num_workers)
+        for task in range(num_tasks)
+    ))
+
+    # Solucionador
     solver = cp_model.CpSolver()
-
-    # Resolver el problema
     status = solver.Solve(model)
 
-    # Mostrar la solución
+    # Imprimir la solución
     if status == cp_model.OPTIMAL:
         print("Asignación óptima encontrada:")
         for worker in range(num_workers):
             for task in range(num_tasks):
                 if solver.Value(assignment[(worker, task)]) == 1:
-                    print(f"Trabajador {worker + 1} realiza Tarea {task + 1}")
+                    print(f"- Trabajador {worker + 1} realiza Tarea {task + 1}")
+        print(f"Tiempo total de trabajo: {solver.Value(total_time)} horas")
     else:
         print("No se encontró una asignación óptima.")
 
 
 
 # CODIGO EJECUTABLE
-
 if __name__ == "__main__":
     assign_tasks()
